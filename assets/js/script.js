e810ebb51b0a089fe564836d596cd54c7a859b96
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-05-08 23:59:31 -0400

    startinng JS

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const timeEl = document.getElementById ("time");
+const dateEl = document.getElementsById ("date");
+const currentWeatherItemEl = document.getElementsById("weather-now");
+
+const API_KEY = d498050fd144f23d29ece9160220d384
