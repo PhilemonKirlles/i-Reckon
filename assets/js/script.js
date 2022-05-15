bc047f0045b1772c0803a8ae811ab3c476db2aba
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-05-15 01:09:15 -0400

    js var const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const timeEl = document.getElementById ("time");
 const dateEl = document.getElementsById ("date");
-const currentWeatherItemEl = document.getElementsById("weather-now");
+const weatherNowEl = document.getElementsById("weather-now");
+const timeZone=getElementById("time-zon")
+const countryEl = document.getElementsById("country")
+const weatherForecastEl = document.getElementsById("weather-forecast")
+const currentTempEl = document.getElementById("current-temp")
 
 const API_KEY = d498050fd144f23d29ece9160220d384
 
