68dc1172d4a87b453f7985bfcf222a32ee7fc074
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-05-15 01:24:17 -0400

    js,setInterval const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 const timeEl = document.getElementById ("time");
 const dateEl = document.getElementsById ("date");
 const weatherNowEl = document.getElementsById("weather-now");
-const timeZone=getElementById("time-zon")
-const countryEl = document.getElementsById("country")
-const weatherForecastEl = document.getElementsById("weather-forecast")
-const currentTempEl = document.getElementById("current-temp")
+const timeZone=getElementById("time-zon");
+const countryEl = document.getElementsById("country");
+const weatherForecastEl = document.getElementsById("weather-forecast");
+const currentTempEl = document.getElementById("current-temp");
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day =time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes ();
+}, 1000);
+
 
 const API_KEY = d498050fd144f23d29ece9160220d384
 
