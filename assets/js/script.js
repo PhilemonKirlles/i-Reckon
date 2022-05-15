74a36dfecd93d276203eea8517f11368b10bdc38
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-05-15 00:39:14 -0400

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,5 @@ const dateEl = document.getElementsById ("date");
 const currentWeatherItemEl = document.getElementsById("weather-now");
 
 const API_KEY = d498050fd144f23d29ece9160220d384
+
+api.openweathermap.org/data/2.5/onecall?lat=38.8&lon=12.09&callback=test
