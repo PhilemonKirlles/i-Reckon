15c8bacf6333db0ba6647e542b0686eec927d193
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-06-17 14:47:48 -0400

    css style, and js plan

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//openweathermap.org/ and create an account and get your API KEY
+//Calling API by geographical coordinates- latitude and longitude
+//Calling API by city ID
+// Declaring the variables
+//API URL + API ID
+// fetching the API
